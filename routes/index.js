bb309465994efd943bdc01c498f77f0c8b55cedc
unknown <himanshupatel06@gmail.com>
2016-02-04 16:15:49 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Us' });
+});
+
+/* GET project page. */
+router.get('/project', function(req, res) {
+  res.render('project', { title: 'Project' });
+});
+
+/* GET social page. */
+router.get('/social', function(req, res) {
+  res.render('social', { title: 'Social' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
 
 module.exports = router;
